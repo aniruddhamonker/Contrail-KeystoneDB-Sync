5a9ab7c6dc963e0928d8477ec536a0424951334f
aamonker <aniruddh.amonker@live.com>
2019-03-02 13:55:19 -0800

    added methods to start and stop contrail services and also to execute "db_json_exim" python script

--- restore_keystone.py
+++ restore_keystone.py
@@ -1,20 +1,12 @@
 import json
 import argparse
-import re
 import time
 import subprocess as sub
 from builtins import property, staticmethod, Exception
-
 from keystoneauth1.identity import v2
 from keystoneauth1 import session
 from keystoneclient.v2_0 import client
 from keystoneauth1 import exceptions as _exc
-
-parser = argparse.ArgumentParser()
-parser.add_argument("json_db_file", type=str, help="Filename of the JSON DB file")
-args = parser.parse_args()
-db_file = args.json_db_file
-
 
 class KeystoneProject:
     def __init__(self, **kwargs):
@@ -25,11 +17,12 @@ class KeystoneProject:
         self.db_file = kwargs.get('db_file', db_file)
 
     def create_new_projects(self, project_names):
-        #type: (KeystoneProject, Generator[Dict]) -> List[Dict]
-        '''
-        This method takes in an instaance of DatabaseSnapshot class as an argument.
+        """
+        This method takes in an instance of DatabaseSnapshot class as an argument.
         db_snapshot here is an instance of DatabaseSnapshot.
-        '''
+        :param self: instance of class KeystoneProject
+        :param project_names: List of Projects dictionary
+        """
         projects = []
         for project in project_names:
             try:
@@ -40,10 +33,12 @@ class KeystoneProject:
                 print("Error when creating new Project {} : Err: {} -- {}".format(project['name'], type(exp), exp))
         return projects
 
-    def delete_keystone_projects(self, projects_list): #type: (Instance[KeystoneProject], List[Dict]) -> None
-        '''
+    def delete_keystone_projects(self, projects_list):
+        """
         This method to be used to cleanup/delete the projects that were created from database snapshot file
-        '''
+        :param projects_list: List of projects(Dict) to delete
+        :return: None
+        """
         for project in projects_list:
             try:
                 self.keystone.tenants.delete(project['new_uuid'])
@@ -56,24 +51,31 @@ class KeystoneProject:
 class DatabaseSnapshot:
     def __init__(self, db_snapshot_file_path):
         """
-
-        :type db_snapshot_file_path: FileObject
+        :type db_snapshot_file_path: Path and filename of customer's database snapshot file
         """
         self.db_snapshot_file_path = db_snapshot_file_path
         with open(self.db_snapshot_file_path) as db_snapshot_file:
             self.json_db_str = db_snapshot_file.read()
             self.json_db_dict = json.loads(self.json_db_str)
 
-    def get_existing_projects(self):  #type: (Instance[DatabaseSnapshot]) -> Generator[Dict]
+    def get_existing_projects(self):
+        """
+        Thi method fetches all the project names and their corresponding UUIDs from customer's DB snapshot file
+        :param self: instance of class DatabaseSnapshot
+        :return: Dictionary generator of project names and corresponding uuids
+        """
         project_dict = self.json_db_dict['cassandra']['config_db_uuid']['obj_fq_name_table']['project']
         for key in project_dict.keys():
             project_name, project_uuid = key.split(":")[1], key.split(":")[2]
             yield {'name': project_name, 'uuid': project_uuid}
 
-    def swap_project_uuids(self, projects_list): #type: ([DatabaseSnapshot], List[Dict] -> Filehandle
+    def swap_project_uuids(self, projects_list): #type: ([DatabaseSnapshot], List[Dict] -> Str
         """
         This method replaces the original uuids of the projects in the snapshot file with the uuids\
         from local Keystone server. The result is saved in the new file with ".changed" extension.
+        :param: self: instance of class DatabaseSnapshot
+        :param: projects_list: List of Projects Dictionary
+        :return: String: Path and filename of the updated file
         """
         #global db_file
         print("Replacing UUIDs of the customer projects in the snapshot file with the new UUIDs from local \
@@ -88,7 +90,7 @@ class DatabaseSnapshot:
                 self.json_db_str = self.json_db_str.replace(old_uuid, new_uuid)
                 self.json_db_str = self.json_db_str.replace(old_uuid_dashed, new_uuid_dashed)
             except Exception as exp:
-                print('Failed to replace UUID for project {}\n{}'.format(project['name']), exp)
+                print("Failed to replace UUID for project {}\n{}".format(project['name'], exp))
         changed_db_file = open("{}.changed".format(self.db_snapshot_file_path), "w")
         changed_db_file.write(self.json_db_str)
         changed_db_file.close()
@@ -103,23 +105,22 @@ class DbJsonEximScript:
         return self.LOADER_SCRIPT
 
     @staticmethod
-    def stop_contrail_services():
-        print("Stopping Contrail config services...\n")
-        stop_config = sub.Popen('service supervisor-config stop || true', shell=True, stdout=sub.PIPE, stderr=sub.PIPE)
-        if stop_config.stderr.read():
-            raise "Unable to stop the supervisor-config process\n{}\n".format(stop_config.stderr.read())
-        print("Stopping Cassandra Database Services...\n")
-        stop_cassandra = sub.Popen('service cassandra stop', shell=True, stdout=sub.PIPE, stderr=sub.PIPE)
-        if stop_cassandra.stderr.read():
-            raise"Unable to stop Cassandra\n{}\n".format(stop_cassandra.stderr.read())
-        print("stopping Zookeeper service...\n")
-        stop_zk = sub.Popen("service zookeeper stop", shell=True, stdout=sub.PIPE, stderr=sub.PIPE)
-        if stop_zk.stderr.read():
-            raise "Unable to stop zookeeper services\n{}\n".format(stop_zk.stderr.read())
-        print('Stopping Kafka service...\n')
-        stop_kafka = sub.Popen("service kafka stop", shell=True, stdout=sub.PIPE, stderr=sub.PIPE)
-        if stop_kafka.stderr.read():
-            raise "Unable to stop Kafka service\n{}\n".format(stop_kafka.stderr.read())
+    def stop_contrail_services(*services):
+        for service in services:
+            print("Stopping {} service...\n".format(service))
+            stop_service = sub.Popen('service {} stop || true'.format(service), shell=True, stdout=sub.PIPE, \
+                                     stderr=sub.PIPE)
+            if stop_service.stderr.read():
+                raise "Unable to stop {} process\n{}\n".format(service, stop_service.stderr.read())
+
+    @staticmethod
+    def start_contrail_services(*services):
+        for service in services:
+            start_service = sub.Popen("service {} start".format(service), shell=True, stdout=sub.Popen,
+                                      stderr=sub.Popen)
+            if start_service.stderr.read():
+                raise "Failed to start {} service\n{}\n".format(service, start_service.stderr.read())
+            time.sleep(20) if (service == 'cassandra') or (service == 'zookeeper') else time.sleep(5)
         return
 
     @staticmethod
@@ -135,20 +136,32 @@ class DbJsonEximScript:
             raise "Failed to delete Cassandra DB\n{}\n".format(cleanup_cassandra.stderr.read())
         return
 
-    @staticmethod
-    def start_zk_and_cassandra_services():
-        print("Starting Zookeeper and Cassandra Services...\n")
-        start_zk = sub.Popen("service zookeeper start", shell=True, stdout=sub.Popen, stderr=sub.Popen)
-        if start_zk.stderr.read():
-            raise "Failed to start Zookeeper service\n{}\n".format(start_zk.stderr.read())
-        start_cassandra = sub.Popen("service cassandra start", shell=True, stdout=sub.Popen, stderr=sub.PIPE)
-        if start_cassandra.stderr.read():
-            raise "Failed to start Cassandra service\n{}\n".format(start_cassandra.stderr.read())
-        time.sleep(25)
-        return
+    def run_db_exim_script(self, db_file_path):
+        """
+        This method runs the LOADER script "db_json_exim" and imports the database from the file supplied as an argument
+        :param db_file_path: filename/path of Database snapshot file whose contents need to be imported into Cassandra
+        :return: None
+        """
+        print("Running db_json_exim.py to import Database from file '{}'".format(db_file_path))
+        script = sub.Popen("python {} {}".format(self.LOADER_SCRIPT, db_file_path), shell=True, stdout=sub.PIPE,\
+                               stderr=sub.PIPE)
+        if script.stderr.read():
+            raise "DB Import Failed\n{}".format(script.stderr.read())
+        else:
+            return script.stdout.read()
 
-    @staticmethod
-    def run_db_exim_script():
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("json_db_file", type=str, help="Path and filename of the JSON DB file")
+    args = parser.parse_args()
+    db_file_path = args.json_db_file
+
+
+
+
+
+
+
 
 
 
